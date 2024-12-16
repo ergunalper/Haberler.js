66d6b764dca566411cfc210ad3d04ad64aaa41c8
alperergun <47082435+ergunalper@users.noreply.github.com>
2024-12-16 22:53:58 +0300

    Create Haberler.js

---
+++ Haberler.js
@@ -0,0 +1,94 @@
+export default
+[
+    {
+        "isim": "Merkez Bankası Başkanı Murat Uysal'dan enflasyon açıklaması",
+        "tanim": "Ekonomi Haberi",
+        "icerik": "TCMB Başkanı Uysal Türkiye Bankalar Birliği üyelerine sunum yaptı. Enflasyon görünümünde iyileşmenin sürdüğüne dikkat çeken Uysal, parasal sıkılığın düzeyinin enflasyondaki düşüşün sürekliliğini sağlayacak şekilde belirleneceğini ifade etti.",
+        "tarih": "29 Ağustos 2019",
+        "url": "http://finans.mynet.com/haber/detay/ekonomi/merkez-bankasi-baskani-murat-uysal-dan-enflasyon-aciklamasi/320825/",
+        "kategori":"Ekonomi",
+        "sehir":"Türkiye",
+        "resim": "https://imgrosetta.mynet.com.tr/file/10193612/10193612-728xauto.jpg"
+    },
+    {
+        "isim": "Emevi Camiinde Tarihi Anlar",
+        "tanim": "Dış Gündem Haberi",
+        "icerik": "Emevi Camii'nde tarihi anlar: Türk bayrağını öpüp Erdoğan'a teşekkür ettiler... Özgür Suriye halkı Esad'ın devrilmesinden sonra ilk cuma namazını kıldı.",
+        "tarih": "13 Aralık 2024",
+        "url": "http://finans.mynet.com/haber/detay/ekonomi/motorin-ve-benzine-zam/319559/",
+        "kategori":"Dış Gündem",
+        "sehir":"Suriye",
+        "resim": "https://image.hurimg.com/i/hurriyet/75/750x422/675c11cb58799e22650420f9.jpg"
+    },
+    {
+        "isim": "Dışişleri Bakanı Hakan Fidan Antony Blinken ile bir araya geldi: 'Önceliğimiz Suriye'de istikrar",
+        "tanim": "Dünya Haberi",
+        "icerik": "Dışişleri Bakanı Hakan Fidan, ABD Dışişleri Bakanı Antony Blinken ile yaptığı kritik görüşme sonrası düzenlenen ortak basın toplantısında gündeme dair değerlendirmelerde bulundu. Bakan Fidan burada yaptığı açıklamada Suriye ve Gazze'deki durumu ABD Dışişleri Bakanı Anthony Blinken ile görüştük. Suriye'nin geleceğinde olası Türk ve Amerikan rolünü tartıştık. Önceliğimiz Suriye'de istikrarın sağlanması. Terörizmin yer bulmaması, DEAŞ'ın PKK'nın orada hakim olmaması önceliklerimiz arasında. Bunlarla ilgili neler yaparız, ortak endişeler neler bunları konuştuk dedi.",
+        "tarih": "29 Ağustos 2019",
+        "url": "https://www.hurriyet.com.tr/dunya/son-dakika-tum-gozler-ankarada-disisleri-bakani-hakan-fidan-abdli-mevkidasi-blinken-ile-bir-araya-geldi-42624596",
+        "kategori":"Ekonomi",
+        "sehir":"Türkiye",
+        "resim": "https://image.hurimg.com/i/hurriyet/75/770x0/675bde73c8198272f9dffb0d.jpg"
+    },
+    {
+        "isim": "Hazine 3 aylık iç borçlanma stratejisini açıkladı",
+        "tanim": "deniz kıyıları, Orta ve Doğu Akden sağanak ve gök gürültülü sağanak bekleniyor.",
+        "icerik": "Hazine ve Maliye Bakanlığı, eylül-kasım döneminde 38,8 milyar liralık iç borç servisine karşılık 30,7 milyar liralık iç borçlanmaya gidecek.",
+        "tarih": "29.08.2019 - 17:33",
+        "url": "http://bigpara.hurriyet.com.tr/haberler/ekonomi-haberleri/hazine-3-aylik-ic-borclanma-stratejisini-acikladi_ID1457083/",
+        "kategori":"Ekonomi",
+        "sehir":"Türkiye",
+        "resim": "http://i2.hurimg.com/i/hurriyet/75/750x422/5d62b93418c7732ac8e343a0.jpg"
+    },
+    {
+        "isim": "Bakan Albayrak'tan Türk Lirası referans faiz oranı açıklaması",
+        "tanim": "Ekonomi Haberi",
+        "icerik": "Bakan Albayrak, Finansal mimarimizde Türk Lirası çalışabilmemiz için Bankalarımızın faiz riskini yönetmelerinde önemli bir eksikliği gidermek üzere TL referans faizi TLREF, BİST bünyesinde oluşturuldu açıklamasında bulundu.",
+        "tarih": "25.08.2019 - 19:37",
+        "url": "http://bigpara.hurriyet.com.tr/haberler/ekonomi-haberleri/bakan-albayrak-tl-referans-faizi-tlref-bist-bunyesinde-olusturuldu_ID1457084/",
+        "kategori":"Ekonomi",
+        "sehir":"Türkiye",
+        "resim": "http://i2.hurimg.com/i/hurriyet/75/750x422/5d62b93418c7732ac8e343a0.jpg"
+    },
+    {
+        "isim": "Petrol ithalatı haziranda azaldı",
+        "tanim": "Ekonomi Haberleri",
+        "icerik": "Türkiye'nin toplam petrol ithalatı haziranda geçen yılın aynı ayına göre yüzde 7 azalarak 3 milyon 151 bin 387 tona geriledi",
+        "tarih": "25.08.2019 - 19:37",
+        "url": "http://bigpara.hurriyet.com.tr/haberler/emtia-haberleri/petrol-ithalati-haziranda-azaldi_ID1457088/",
+        "kategori":"Ekonomi",
+        "sehir":"Türkiye",
+        "resim": "http://i2.hurimg.com/i/hurriyet/75/750x422/5d62b93418c7732ac8e343a0.jpg"
+    },
+    {
+        "isim": "İngiltere Kralı 3. Charles, kanseri yenmek için kırmızı eti hayatından tamamen çıkardı",
+        "tanim": "Sağlık Haberleri",
+        "icerik": "Kral Charles ın iyileşmesini hızlandırmak ve kanseri yenmek amacıyla kırmızı eti tamamen diyetinden çıkardığı ortaya çıktı.Kral'a, ocak ayında kanser teşhisi konuldu.Tedavisi süresince resmi devlet görevlerine devam etti ancak 3 ay sonra halka açık etkinliklere katılmaya başladı.Kral Charles, her zaman ağır gıdalar yerine basit ve organik besinleri tercih etti, her zaman sağlıklı yaşam konusunda kararlı oldu ancak şimdi sağlığını daha da ciddiye alıyor.",
+        "tarih": "25.08.2019 - 19:37",
+        "url": "https://www.hurriyet.com.tr/aile/ingiltere-krali-3-charles-kanseri-yenmek-icin-kirmizi-eti-hayatindan-tamamen-cikardi-uzmanlar-uyariyor-beyaz-ete-gore-10-ka-42622112/",
+        "kategori":"Ekonomi",
+        "sehir":"Türkiye",
+        "resim": "https://image.hurimg.com/i/hurriyet/75/750x422/675bf09490076a49d9182bd5.jpg"
+    },
+    {
+        "isim": "100 milyar dolar için ilk adım atıldı",
+        "tanim": "Ekonomi Haberleri",
+        "icerik": "ABD Ticaret Bakanı Wilbur Ross, 6-11 Eylül tarihlerinde Türkiye’ye yapacağı resmi ziyaret öncesinde Türk Amerikan İş Konseyi (TAİK) başkanı Mehmet Ali Yalçındağ’ı Washington’da kabul etti. Cumhurbaşkanı Erdoğan’ın ABD ile ‘100 milyar dolar’ ticaret hedefi projesinin startı Washington’da verildi",
+        "tarih": "31.08.2019 - 11:08",
+        "url": "http://bigpara.hurriyet.com.tr/haberler/ekonomi-haberleri/100-milyar-dolar-icin-ilk-adim-atildi_ID1457094/",
+        "kategori":"Ekonomi",
+        "sehir":"Türkiye",
+        "resim": "http://i2.hurimg.com/i/hurriyet/75/750x422/5d62b93418c7732ac8e343a0.jpg"
+    },
+    {
+        "isim": "Yurt dışına açılmak isteyen firmalara DEİK'ten destek",
+        "tanim": "Ekonomi Haberleri",
+        "icerik": "DEİK Başkanı Olpak, “İhracat Ana Planına Destek Paketi kapsamında DEİK aidatında ilk yıl için yüzde 60’lık bir indirim yaptık. Toplam aidatın yüzde 40’ını ödeyerek bize üye olabilecekler. İş konseylerinin kendi faaliyetleri için topladığı farklı aidat meblağı var. Oradaki aidat miktarında ise yüzde 20’lik bir indirim sağladık.” dedi.",
+        "tarih": "30.08.2019 - 11:33",
+        "url": "http://bigpara.hurriyet.com.tr/haberler/ekonomi-haberleri/yurt-disina-acilmak-isteyen-firmalara-deikten-destek_ID1457090/",
+        "kategori":"Ekonomi",
+        "sehir":"Türkiye",
+        "resim": "http://i2.hurimg.com/i/hurriyet/75/750x422/5d62b93418c7732ac8e343a0.jpg"
+    },
+
+]
